67f01c788f42fd975b97803c31dbd743555e4779
Watit Thammarat <tongnakub@hotmail.com>
2019-01-11 00:12:40 +0700

    added signup & login functionalities

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const mongoose = require('mongoose');
+// const cors = require('cors');
 
 const schema = require('./grapql/schema');
 const rootValue = require('./grapql/resolver');
 const isAuth = require('./middleware/is-auth');
 
-const PORT = 3000;
+const PORT = 5000;
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(isAuth);
--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,8 +1,88 @@
 import React, { Component } from 'react';
 
+import './Auth.css';
+
 class Auth extends Component {
+  constructor(props) {
+    super(props);
+    this.emailEl = React.createRef();
+    this.passwordEl = React.createRef();
+    this.state = {
+      isLogin: true
+    };
+  }
+
+  submitHandler = async e => {
+    e.preventDefault();
+    const email = this.emailEl.current.value;
+    const password = this.passwordEl.current.value;
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+    let body = {
+      query: `
+        query {
+          login(email: "${email}", password: "${password}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `
+    };
+    if (!this.state.isLogin) {
+      body = {
+        query: `
+        mutation {
+          createUser(userInput: { email: "${email}", password: "${password}" }) {
+            _id
+            email
+          }
+        }
+      `
+      };
+    }
+    try {
+      const res = await fetch('http://localhost:5000/graphql', {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const statuses = [200, 201];
+      if (!statuses.includes(res.status)) {
+        throw new Error('Failed');
+      }
+      const json = await res.json();
+      console.log(json);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   render() {
-    return <div>The Auth Page</div>;
+    return (
+      <form className="auth-form" onSubmit={this.submitHandler}>
+        <div className="form-control">
+          <label htmlFor="email">E-Mail</label>
+          <input ref={this.emailEl} type="email" id="id" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input ref={this.passwordEl} type="password" id="password" />
+        </div>
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+          <button
+            type="button"
+            onClick={() => this.setState(prev => ({ isLogin: !prev.isLogin }))}
+          >
+            Switch to {this.state.isLogin ? 'Signup' : 'Login'}
+          </button>
+        </div>
+      </form>
+    );
   }
 }
 
